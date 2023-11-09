fbc60a6bb1ba1fd1f68ab408a5a89b698fbd3b39
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-09 17:45:47 +0530

    1)BASIC : Hello_World.cs
    Simple program in exercism to display hello world.

---
+++ 1)Hello_World.cs
@@ -0,0 +1,8 @@
+//required to add library functions and name space
+//using Systems;
+//namespace Program;
+public static class HelloWorld
+{
+    public static string Hello() => "Hello, World!";
+    
+}
